4ae94505d4bd2b7473398766b2a8b07afdd9b492
Estheef <stefyvs1995@gmail.com>
2019-07-27 17:13:49 -0500

    Templates modificados para mostrar post desde base de datos.

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from django.utils import timezone
+from django.shortcuts import render
+from .models import Post
 
 
 class Post(models.Model):
